227e86e002bb553e0b8f04d5b7ae54d1d65f2b9c
Ninghang Mei <meininghang@foxmail.com>
2018-03-09 22:10:24 +0800

    update

--- 豆瓣影评实战.py
+++ 豆瓣影评实战.py
@@ -308,3 +308,403 @@ T_3_comment.to_csv('/Users/meininghang/Desktop/T_3_comment.csv') #导出
         从其中可得出两种结论:
         1.评分人整体上对电影持认同态度,情绪平均值高于0.5;
         2.星级和情绪持正相关,即评分越高,印象也较为正面.
+# ## 主题和文本相似度
+
+# In[1]:
+
+
+import pandas as pd
+import jieba
+import gensim #构建依赖库
+from gensim import corpora
+
+
+# In[2]:
+
+
+#加载数据
+T_3_comment = pd.read_csv('/Users/meininghang/douban-movie-sets-anyalase/T_3_comment.csv')
+T_3_comment['评论内容'][:5] #查看内容
+
+
+# In[6]:
+
+
+with open("/Users/meininghang/Downloads/stopwords-master/四川大学机器智能实验室停用词库.txt") as f: #停用词
+    stop_words = f.read().splitlines()
+
+
+# In[8]:
+
+
+comment = []
+for i in T_3_comment.评论内容:
+    comment_seg = [] #加载单个评论分词结果
+    segs = jieba.cut(i) #分词
+    for seg in segs:
+        if seg not in stop_words:# 过滤
+            comment_seg.append(seg)
+    comment.append(comment_seg)
+
+
+# In[11]:
+
+
+comment[0] #查验
+
+
+# In[12]:
+
+
+#TF-IDF
+dictionary = corpora.Dictionary(comment)
+
+
+# In[13]:
+
+
+cipin = [dictionary.doc2bow(comm) for comm in comment]
+cipin
+
+
+# In[18]:
+
+
+from gensim import models
+from gensim.corpora import corpus
+tf_idf_model = models.TfidfModel(cipin)
+tfidf = tf_idf_model[cipin]
+#tf_idf_mat = #corpus2dense(tfidf,len(dictionary))
+
+
+# In[19]:
+
+
+model = gensim.models.Word2Vec(comment,size=1000,window=5,min_count=5)
+
+
+# In[20]:
+
+
+model['评论']
+
+
+# In[36]:
+
+
+model.similarity('剧','电影')
+
+
+# In[ ]:
+
+
+#jeibatf-idf示例
+'''基于 TF-IDF 算法的关键词抽取
+import jieba.analyse
+
+jieba.analyse.extract_tags(sentence, topK=20, withWeight=False, allowPOS=())
+sentence 为待提取的文本
+topK 为返回几个 TF/IDF 权重最大的关键词，默认值为 20
+withWeight 为是否一并返回关键词权重值，默认值为 False
+allowPOS 仅包括指定词性的词，默认值为空，即不筛选
+jieba.analyse.TFIDF(idf_path=None) 新建 TFIDF 实例，idf_path 为 IDF 频率文件'''
+
+
+# In[23]:
+
+
+import jieba
+import jieba.analyse
+
+
+# In[88]:
+
+
+k = jieba.analyse.extract_tags(T_3_comment.评论内容[1],topK=2,withWeight=True)
+
+
+# In[89]:
+
+
+k
+
+
+# In[41]:
+
+
+k[1][0] #type==str
+
+
+# In[42]:
+
+
+model.similarity(k[0][0],k[1][0])
+
+
+# In[48]:
+
+
+for i in T_3_comment.评论内容:
+    k = jieba.analyse.extract_tags(i,topK=2,)
+
+
+# In[52]:
+
+
+k
+
+
+# In[60]:
+
+
+from gensim.similarities import MatrixSimilarity
+
+
+# In[62]:
+
+
+sim_index = MatrixSimilarity(comment)
+
+
+# In[63]:
+
+
+sim_index[comment[0]]
+
+
+# In[64]:
+
+
+sort_sims = sorted(enumerate(sim_index[comment[0]]), key=lambda item: item[1],reverse=True)  # 利用 sorted 函数按照相似度从大到小排序
+print(sort_sims[0:10])  # 输出最为相似的前 10 个文档编号及相似度
+
+
+# In[68]:
+
+
+for j in [i[0] for i in sort_sims[0:10]]:
+    print(j,"\n",T_3_comment.评论内容[j])   #输出原始文档内容，查看检索效果
+
+        首先回溯第一篇影评的直观感受是"讽刺',其情绪得分较低,而此时返回的相近文本具有相同的感受,即"不满'
+# In[70]:
+
+
+T_3_comment.情绪[0]
+
+
+# ## 可视化
+
+# In[97]:
+
+
+get_ipython().system('pip3 install pyldavis')
+
+
+# In[98]:
+
+
+import pyLDAvis
+import pyLDAvis.gensim
+from gensim import corpora,models
+from gensim.models.ldamodel import LdaModel
+
+
+# In[106]:
+
+
+with open('/Users/meininghang/Downloads/stopwords-master/四川大学机器智能实验室停用词库.txt') as f:
+    stop_words = f.read().splitlines()
+    comment = []
+    for i in T_3_comment.评论内容:
+        segment = []
+        segs = jieba.cut(i)
+        for seg in segs:
+            if seg not in stop_words:
+                segment.append(seg)
+        comment.append(segment)
+
+
+# In[107]:
+
+
+dictionary = corpora.Dictionary(comment)
+corpus = [dictionary.doc2bow(commen) for commen in comment]
+
+
+# In[108]:
+
+
+tfidf_model = models.TfidfModel(corpus)
+corpus_tfidf = tfidf_model[corpus]
+
+
+# In[109]:
+
+
+lda = LdaModel(corpus=corpus_tfidf,id2word=dictionary,num_topics=3)
+
+
+# In[118]:
+
+
+lda.show_topics(9)
+
+
+# In[120]:
+
+
+vis_data = pyLDAvis.gensim.prepare(lda,corpus,dictionary)
+vis_data
+
+
+# In[123]:
+
+
+pyLDAvis.display(vis_data)
+
+
+# In[140]:
+
+
+k = T_3_comment.评论时间
+k = str(k)
+print(k,'\n')
+
+
+# In[146]:
+
+
+year = []
+month = []
+day = []
+
+for i in T_3_comment.评论时间:
+    year.append(i.split('-')[0])
+    month.append(i.split('-')[1])
+    day.append(i.split('-')[2])
+
+T_3_comment['year'] = year
+T_3_comment['month'] = month
+T_3_comment['day'] = day
+
+T_3_comment['count'] = len(T_3_comment) * [1]
+T_3_comment.head()
+
+
+# In[147]:
+
+
+dataset_ym = pd.pivot_table(T_3_comment,values='count',
+                            index = 'year',columns='month',aggfunc='sum')
+dataset_ym
+
+
+# In[148]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+import seaborn as sea
+sea.heatmap(dataset_ym,linewidths=.5)
+
+        表示不同年份各月份累计评论数热图
+# In[149]:
+
+
+sea.heatmap(dataset_ym,annot=True,fmt="n",linewidths=.5)
+
+
+# In[151]:
+
+
+title[2]
+
+
+# In[155]:
+
+
+T_3_comment['words'] = comment
+
+
+# In[156]:
+
+
+T_3_comment['word_count'] = [i.count('评论') for i in T_3_comment['words']]
+
+
+# In[157]:
+
+
+dataset_md_words=pd.pivot_table(T_3_comment, values = 'word_count', index = 'month', columns = 'day',aggfunc='sum')
+
+
+# In[158]:
+
+
+dataset_md_words
+
+
+# In[159]:
+
+
+sea.heatmap(dataset_md_words,linewidths=.5)
+
+
+# In[160]:
+
+
+from datetime import datetime
+
+
+# In[181]:
+
+
+T_3_comment.评论时间[1]
+
+
+# In[183]:
+
+
+time = datetime.strptime(T_3_comment['评论时间'][0], '%Y-%m-%d %H:%M:%S')
+
+
+# In[185]:
+
+
+print(time)
+
+
+# In[192]:
+
+
+T_3_comment['具体时间'] = [datetime.strptime(i,'%Y-%m-%d %H:%M:%S') for i in T_3_comment['评论时间']]
+
+
+# In[188]:
+
+
+T_3_comment.具体时间.head()
+
+
+# In[193]:
+
+
+T_3_comment.具体时间.value_counts()
+
+
+# In[194]:
+
+
+T_3_comment.评论时间.value_counts().plot(kind='line', rot=0, figsize=(14, 8))
+
+
+# In[197]:
+
+
+group_word = T_3_comment.groupby(['评论时间'])['word_count'].sum()
+
+
+# In[198]:
+
+
+group_word.plot(kind='line',figsize=(14, 8))
+
